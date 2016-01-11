225557ef73b53480f680702da31e36bc8f78f53a
brianthelam <blam.sh@mysummitps.org>
2016-01-11 08:42:15 +0000

    maybe done

--- info.js
+++ info.js
@@ -8,17 +8,17 @@ var info3 = document.getElementById("info3");
 var info4 = document.getElementById("info4");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
+    info1.innerHTML = "A huge advantage Android has against Apple is how customizable their phones are. If there is something you don't like about your Android phone, its customizability provides quick solutions. For example, if you are not satisfied by its default keyboard, then you can easily download a third-party keyboard. With Android phones, you can download widgets that allow you to customize your phone and provide extra functionality. One such widget can be a weather widget that displays the predicted weather for your area.";
 });
                        
 item2.addEventListener("click", function(){
-    info2.innerHTML = "Item 2 other stuff";
+    info2.innerHTML = "The convenience of Android phones using micro USB chargers is another great advantage. Micro USB chargers are so universal that if you forgot your charger while hitting the town, chances are that someone will have a micro USB charger. So what does Apple have? They have their own lightning cables, which only work for other Apple devices. They are inconvenient and require you to spend extra money on chargers, especially because of the terrible quality of the chargers.";
 });
 
 item3.addEventListener("click", function(){
-    info3.innerHTML = "3";
+    info3.innerHTML = "A large number of Android phones offer expandable memory. If you are ever running low on memory, you can just take off the back casing, and replace your old micro SD card with one that has greater storage. Upgrading from 16 GB to 32 Gb will probably cost you around 10 dollars. What happens when you want more storage on an iPhone? Apple is more than happy to charge you $100 or more for a new phone just to upgrade storage. It's quite obvious which side wins in this situation.";
 });
 
 item4.addEventListener("click", function(){
-    info4.innerHTML = "1";
+    info4.innerHTML = "Finally, we get to the hardware debate. I will be comparing the pervious two main flagship phones from each company, the Samsung Galaxy S5 and the Apple iPhone 6. After a bit of research, one will find that the S5 far surpasses the iPhone 6 in the hardware department. The S5 features a quad-core 2.5 GHz processor and 2 GB of RAM, while the 6 contains only a dual-core 2 GHz processor and 1 GB of RAM. With those specs, the S5 is definitely more powerful. The S5 offers a 5.1 inch screen with 1920 x 1080 screen resolution and 432 ppi. Meanwhile, the 6 only has a 4.7 inch screen with 1334 x 750 screen resolution and 326 ppi. Apple always advertises how superior their iPhone cameras are, but when compared to Android, Apple fans will be very disappointed. The 6's rear camera is 8 MP and is capable of 1080p HD video, while the S5's rear camera is a whopping 16 MP, double the iPhone's, and is capable of 4K HD video. There are a plethora of other hardware advantages the S5 has over the 6, such as superior battery capacity, having a removable battery, and being water resistant.";
 });
